98268da34a0be201719df3833a20039ef5ba6c40
Chris Anderson <canderson@paxos.com>
2018-01-30 11:11:48 -0500

    add --force-in-progress flag to let migrate steamroll things stuck in progress

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-VERSION = '0.3.2'
+VERSION = '0.3.3'
 
 setup(name='cassandra-migrate',
       packages=['cassandra_migrate'],
       version=VERSION,
       description='Simple Cassandra database migration program.',
       long_description=open('README.rst').read(),
-      url='https://github.com/Cobliteam/cassandra-migrate',
-      download_url='https://github.com/Cobliteam/cassandra-migrate/archive/{}.tar.gz'.format(VERSION),
+      url='https://github.com/paxos-bankchain/migrate-cassandra',
+      download_url='https://github.com/paxos-bankchain/migrate-cassandra/archive/{}.tar.gz'.format(VERSION),
       author='Daniel Miranda',
       author_email='daniel@cobli.co',
       license='MIT',
